ece469fe1074d62d065be98848bea39644e791fd
jiangshuilong <jiangshuilong@cestc.cn>
2021-09-22 16:53:16 +0800

    切面和切面中获取上下文中的参数

---
+++ springboot-mybits-dubbo-nacos-service/src/main/java/com/cc/aspect/DemoAspect.java
@@ -0,0 +1,48 @@
+package com.cc.aspect;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Modifier;
+
+@Component
+@Aspect
+public class DemoAspect {
+
+    @Pointcut("within(com.cc.service.*)")
+    private void doDemo() {
+        System.out.println("切面");
+    }
+
+    @Before("doDemo()")
+    private void beforeDemo(JoinPoint joinPoint) {
+
+        System.out.println("切面执行前");
+        System.out.println("目标方法名为:" + joinPoint.getSignature().getName());
+        System.out.println("目标方法所属类的简单类名:" + joinPoint.getSignature().getDeclaringType().getSimpleName());
+        System.out.println("目标方法所属类的类名:" + joinPoint.getSignature().getDeclaringTypeName());
+        System.out.println("目标方法声明类型:" + Modifier.toString(joinPoint.getSignature().getModifiers()));
+        //获取传入目标方法的参数
+        Object[] args = joinPoint.getArgs();
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("第" + (i + 1) + "个参数为:" + args[i]);
+        }
+        System.out.println("被代理的对象:" + joinPoint.getTarget());
+        System.out.println("代理对象自己:" + joinPoint.getThis());
+    }
+
+    @After("doDemo()")
+    private void afterDemo() {
+        System.out.println("切面执行后");
+    }
+
+    @Around("doDemo()")
+    public void sayAround(ProceedingJoinPoint pjp) throws Throwable {
+        System.out.println("注解类型环绕通知..环绕前");
+        pjp.proceed();//执行方法
+        System.out.println("注解类型环绕通知..环绕后");
+    }
+}
